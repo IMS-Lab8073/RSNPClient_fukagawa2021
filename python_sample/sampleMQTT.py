07bd97e3133c9e77fd7f5da334ef6832b77fca1d
koichirokato <k.kato@tvisioninsights.com>
2021-08-11 16:09:52 +0900

    add python sample files

---
+++ python_sample/sampleMQTT.py
@@ -0,0 +1,36 @@
+import MQTTClient
+import time
+import json
+
+mqttc = MQTTClient.MyMQTTClass()
+# start subscribe
+rc = mqttc.run("localhost","fromServer/Velocity")
+
+i=0
+while True:
+    # send data format (dictionary)
+    i+=1
+    # send_data = {"data_type":"count", "data":i}
+    # send_data = {"location_id":"2"}
+    send_data = {"location_id":"2", "data":[{"function_id":1,"data":2},{"function_id":2,"data":3}]}
+    # print(send_data["data"][0]["data"])
+    send_data_json = json.dumps(send_data)
+    print("publish data : " + str(send_data_json))
+    
+    # publish data
+    mqttc.publish_message("localhost", "toUnit/Robotdata", send_data_json)
+    
+    # subscribe data (if isNew() is True)
+    # data format : {"robotID":"", "vx":"", "va":"", "option":"", "timestamp":""}
+    if mqttc.isNew(): 
+        print(mqttc.recieve_data)
+        recieve_data      = mqttc.recieve_data
+        recieve_data_dict = json.loads(recieve_data)
+        
+        vx = recieve_data_dict["vx"]
+        va = recieve_data_dict["va"]
+        option = recieve_data_dict["option"]
+
+        print("recieve data : ")
+        print("vx : "+str(vx)+", va : "+str(va)+", option : "+option)
+    time.sleep(5)
